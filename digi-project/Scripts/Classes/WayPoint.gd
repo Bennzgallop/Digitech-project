b84ef14658532d3cec3eaefb4e7470ed583b4e5a
Bennzgallop <96319813+Bennzgallop@users.noreply.github.com>
2026-08-26 15:06:08 +1200

    Changed color to colour

--- digi-project/Scripts/Classes/WayPoint.gd
+++ digi-project/Scripts/Classes/WayPoint.gd
@@ -1,34 +1,33 @@
 class_name Waypoint
 extends MeshInstance3D
 
-@export var color : Color = Color.WHITE
+@export var colour : Color = Color.WHITE
 @export var Area : Area3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var mesh = MeshInstance3D.new()
 	add_child(mesh)
-
+	
 	var sphere = SphereMesh.new()
 	sphere.radius = 0.3
 	sphere.height = 0.6
 	mesh.mesh = sphere
 	var material = ShaderMaterial.new()
 	var shader = Shader.new()
-
+	
 	shader.code = """
 	shader_type spatial;
 	render_mode unshaded, depth_test_disabled, depth_draw_never;
-
+	
 	uniform vec4 marker_color : source_color;
-
+	
 	void fragment() {
 		ALBEDO = marker_color.rgb;
 		ALPHA = marker_color.a;
 	}
 	"""
 	material.shader = shader
-	material.set_shader_parameter("marker_color", color)
-
+	material.set_shader_parameter("marker_color", colour)
 	mesh.material_override = material
 	
 	Area.connect("area_entered",self._on_area_entered)
