824f8619bb24a2d3ba93ac8bf0d2ef84714802c5
Bennzgallop <96319813+Bennzgallop@users.noreply.github.com>
2026-09-26 19:10:00 +1200

    heart moniter good now i think

--- digi-project/Scenes/heart_moniter.gd
+++ digi-project/Scenes/heart_moniter.gd
@@ -12,11 +12,24 @@ extends Control
 @onready var regular2connecthingonto = $Regular2/ConnectThingonto
 
 
+<<<<<<< Updated upstream
 @onready var fast1 = $Regular3
 @onready var fast1connecthingonto = $Regular3/ConnectThingonto
 
 @onready var fast2 = $Regular4
 @onready var fast2connecthingonto = $Regular4/ConnectThingonto 
+=======
+@export var fast1 : Control
+@export var fast1connecthingonto : Control
+
+@export var fast2 : Control
+@export var fast2connecthingonto : Control 
+
+var reg1 = false
+var reg2 = false
+var fas1 = false
+var fas2 = false
+>>>>>>> Stashed changes
 
 # Called when the node enters the scene tree for the first time. 
 func _ready() -> void:
@@ -26,9 +39,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if mainui.character.health > 49:
+<<<<<<< Updated upstream
 		var currthing = "reg1"
+=======
+		reg1 = false
+		reg2 = false
+>>>>>>> Stashed changes
 		regular1.visible = true
 		regular2.visible = true
+		fast1.visible = false
+		fast2.visible = false
+		fast1.position.x -= 1
+		fast2.position.x -= 1
 		regular1.position.x -= 0.5
 		regular2.position.x -= 0.5
 		fast1.position.x -= 0.5
@@ -48,13 +70,33 @@ func _physics_process(delta: float) -> void:
 		if regular2:
 			if regular2connecthingonto.global_position.x <= endpoint.global_position.x:
 				regular2.global_position.x = regular1connectthingonto.global_position.x
+<<<<<<< Updated upstream
 				currthing = "reg2"
 	elif mainui.character.health > 24:
 		print("Else")
+=======
+		if fast1:
+			if fast1connecthingonto.global_position.x <= endpoint.global_position.x:
+				fast1.global_position.x = fast2connecthingonto.global_position.x
+				fas1 = true
+			if fas1 == false:
+				fast1.position.x -= 0.5
+		if fast2:
+			if fast2connecthingonto.global_position.x <= endpoint.global_position.x:
+				fast2.global_position.x = fast1connecthingonto.global_position.x
+				fas2 = true
+			if fas2 == false:
+				fast2.position.x -= 0.5
+	elif mainui.character.health > 24:
+		print("Else")
+		fas1 = false
+		fas2 = false
+>>>>>>> Stashed changes
 		fast1.visible = true
 		fast2.visible = true
 		regular1.visible = false
 		regular2.visible = false
+<<<<<<< Updated upstream
 		fast1.position.x -= 1.5
 		fast2.position.x -= 1.5
 		regular1.position.x -= 1.5
@@ -65,11 +107,29 @@ func _physics_process(delta: float) -> void:
 		if fast2:
 			if fast2connecthingonto.global_position.x <= endpoint.global_position.x:
 				fast2.global_position.x = fast1connecthingonto.global_position.x
+=======
+		fast1.position.x -= 1
+		fast2.position.x -= 1
+		regular1.position.x -= 0.5
+		regular2.position.x -= 0.5
+>>>>>>> Stashed changes
 		if regular1:
 			if regular1connectthingonto.global_position.x <= endpoint.global_position.x:
 				regular1.visible = false
 				regular1.global_position.x = regular2connecthingonto.global_position.x
+				reg1 = true
+			if reg1 == false:
+				regular1.position.x -= 1
 		if regular2:
 			if regular2connecthingonto.global_position.x <= endpoint.global_position.x:
 				regular2.visible = false
 				regular2.global_position.x = regular1connectthingonto.global_position.x
+				reg2 = true
+			if reg2 == false:
+				regular2.position.x -= 1
+		if fast1:
+			if fast1connecthingonto.global_position.x <= endpoint.global_position.x:
+				fast1.global_position.x = fast2connecthingonto.global_position.x
+		if fast2:
+			if fast2connecthingonto.global_position.x <= endpoint.global_position.x:
+				fast2.global_position.x = fast1connecthingonto.global_position.x
